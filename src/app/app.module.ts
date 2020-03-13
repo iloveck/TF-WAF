caac4fc8be8c778335535c7aab3c81b3d4533b50
Eric Schiller <c_eschiller@costco.com>
2020-03-13 16:27:47 -0700

    init

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { registerLocaleData } from '@angular/common';
+import localeFr from '@angular/common/locales/fr';
+
+registerLocaleData(localeFr, 'fr-FR');
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/paid-membership/paid-membership.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PaidMembershipComponent } from './paid-membership.component';
+
+
+
+@NgModule({
+  declarations: [PaidMembershipComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class PaidMembershipModule { }
