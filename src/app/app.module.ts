de5ec701492fff27e7a774b55dba34c48915a9b2
Eric Schiller <c_eschiller@costco.com>
2020-03-15 22:01:11 -0700

    Sample component and service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { registerLocaleData } from '@angular/common';
 import localeFr from '@angular/common/locales/fr';
+import { SampleModule } from './sample/sample.module';
+import { MemberModule } from './member/member.module';
+import { PaidMembershipModule } from './paid-membership/paid-membership.module';
+import { RewardsModule } from './rewards/rewards.module';
+import { SearchModule } from './search/search.module';
 
 registerLocaleData(localeFr, 'fr-FR');
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
+    MemberModule,
+    PaidMembershipModule,
+    RewardsModule,
+    SearchModule,
+    SampleModule,
     BrowserModule,
     AppRoutingModule
   ],
