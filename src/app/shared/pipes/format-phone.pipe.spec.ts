de0da2f76198df3f4cabc774ed953584b925e8a8
Atish Surana <c_asurana@costco.com>
2020-03-26 16:04:23 +0000

    Added test format phone and corrected sample service

--- src/app/shared/pipes/format-phone.pipe.spec.ts
+++ src/app/shared/pipes/format-phone.pipe.spec.ts
@@ -5,4 +5,12 @@ describe('FormatPhonePipe', () => {
     const pipe = new FormatPhonePipe();
     expect(pipe).toBeTruthy();
   });
+  it('Format phone number without country code', () => {
+    const pipe = new FormatPhonePipe();
+    expect(pipe.transform("9999999999")).toBe("(999) 999-9999");
+  });
+  it('Format phone number with country code', () => {
+    const pipe = new FormatPhonePipe();
+    expect(pipe.transform("19999999999")).toBe("+1 (999) 999-9999");
+  });
 });
