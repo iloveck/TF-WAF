821687f3f192ad7feff8f309d179d8e5d71819e0
Nirav Vyas <c_niravvyas@costco.com>
2020-03-30 23:17:46 +0000

    Added top bar component to shared components

--- src/app/shared/components/components.module.ts
+++ src/app/shared/components/components.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
 
 
 
 @NgModule({
-  declarations: [PageNotFoundComponent],
+  declarations: [PageNotFoundComponent, TopBarComponent], 
   imports: [
     CommonModule
   ],
