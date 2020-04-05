5fb007d5033aa83b216e4009f935bc942932fe99
Eric Schiller <c_eschiller@costco.com>
2020-04-05 13:56:10 -0700

    person model created and added to member

--- src/app/member/components/create-member/create-member.component.ts
+++ src/app/member/components/create-member/create-member.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { PersonService } from 'src/app/services/person.service';
 import { TempData } from './tempdata';
+import { Person } from 'src/app/shared/models/person.model';
 
 @Component({
   selector: 'app-create-member',
@@ -10,39 +11,58 @@ import { TempData } from './tempdata';
   providers: [TempData]
 })
 export class CreateMemberComponent implements OnInit {
-  person: any;
+  person = new Person(
+    {
+      firstName: '',
+      middleInitial: '',
+      lastName: '',
+      birthdate: '',
+      languageCode: '',
+      generationSuffix: ''
+    },
+    [{ phoneNumber: '', phoneType: '' }, { phoneNumber: '', phoneType: '' }],
+    [{ emailAddress: '', emailTypeCode: '' }, { emailAddress: '', emailTypeCode: '' }],
+    [
+      {
+        line1: '',
+        line2: '',
+        line3: '',
+        city: '',
+        stateProvinceCode: '',
+        postalCode: '',
+        countryCode: ''
+      }
+    ],
+    [
+      { idNumber: '', idTypeCode: '', stateProvinceCode: '', countryCode: '' }
+    ],
+    true,
+    'Desktop',
+    'u5666',
+    '110'
+  );
   additionalAddressLine: number;
   meta: any;
 
-  constructor(private personService: PersonService, private tempData: TempData) { }
+constructor(private personService: PersonService, private tempData: TempData) { }
 
-  addAddressLine(): void {
+addAddressLine(): void {
     if (this.additionalAddressLine < 2) {
       this.additionalAddressLine++;
     }
   }
 
-  continue(): void {
+continue(): void {
     console.log(this.person);
     this.personService.createPerson(this.person).subscribe(result => {
       console.log(result);
     });
   }
 
-  ngOnInit(): void {
+ngOnInit(): void {
     this.meta = this.tempData.getData();
-    this.person = {
-      profile: {},
-      phone: [{}, {}],
-      email: [{}, {}],
-      address: [{}],
-      identity: [{}],
-      baSameAsMa: true,
-      createdByChannel: 'DESKTOP',
-      createdByUser: 'U66666666',
-      createdAtLocation: '110'
-    };
     this.additionalAddressLine = 0;
+    console.log(this.person);
   }
 
 }
---
+++ src/app/shared/models/person.model.ts
@@ -0,0 +1,55 @@
+export class Person {
+  constructor(
+    public profile: {
+      firstName: string;
+      middleInitial: string;
+      lastName: string;
+      birthdate: string;
+      languageCode: string;
+      generationSuffix: string;
+    },
+    public phone: [
+      {
+        phoneNumber: string;
+        phoneType: string;
+      },
+      {
+        phoneNumber: string;
+        phoneType: string;
+      }
+    ],
+    public email: [
+      {
+        emailAddress: string;
+        emailTypeCode: string;
+      },
+      {
+        emailAddress: string;
+        emailTypeCode: string;
+      }
+    ],
+    public address: [
+      {
+        line1: string;
+        line2: string;
+        line3: string;
+        city: string;
+        stateProvinceCode: string;
+        postalCode: string;
+        countryCode: string;
+      }
+    ],
+    public identity: [
+      {
+        idTypeCode: string;
+        idNumber: string;
+        stateProvinceCode: string;
+        countryCode: string;
+      }
+    ],
+    public baSameAsMa: boolean,
+    public createdByChannel: string,
+    public createdByUser: string,
+    public createdAtLocation: string
+  ) {}
+}
