c50f08cdda12e90f3020fe5b99eee63b4bc30970
Eric Schiller <c_eschiller@costco.com>
2020-04-06 11:49:30 -0700

    fix api url;

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
 
-  createMemberUrl: 'https://mglo-membership-api-dev.azurewebsites.net/api/v1/person',
+  createMemberUrl: 'https://mglo-member-api-dev.azurewebsites.net/api/v1/person',
   createMembershipUrl: 'https://mglo-membership-api-dev.azurewebsites.net/api/v1/account',
   awarenessCodesUrl: 'https://mglo-membership-api-dev.azurewebsites.net/api/v1/awarenesscodes?code=efgh5678',
   searchBaseURL: 'https://mglo-search-api-dev.azurewebsites.net/api/v1/'
--- src/environments/environment.stage.ts
+++ src/environments/environment.stage.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  createMemberUrl: 'https://mglo-membership-api-dev.azurewebsites.net/api/v1/person',
+  createMemberUrl: 'https://mglo-member-api-dev.azurewebsites.net/api/v1/person',
   createMembershipUrl: 'https://mglo-membership-api-dev.azurewebsites.net/api/v1/account',
   awarenessCodesUrl: 'https://mglo-membership-api-dev.azurewebsites.net/api/v1/awarenesscodes?code=efgh5678'
 };
